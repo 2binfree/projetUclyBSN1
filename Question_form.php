af6cd5e3375cec06c19e9776ff187035a918d771
Heidi SOUIBKI <heidi.souibki@esqese.ucly.fr>
2019-05-02 16:09:57 +0200

    css

--- Question_form.php
+++ Question_form.php
@@ -19,6 +19,8 @@ if (!empty($_POST) && $_POST['formType'] == "question") {
     <meta charset="utf-8">
     <title>Universalitis</title>
     <link rel="stylesheet" href="Assets/css/Question_form.css">
+    <link rel="stylesheet" href="Assets/css/header.css">
+    <link rel="stylesheet" href="Assets/css/footer.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
 </head>
 
